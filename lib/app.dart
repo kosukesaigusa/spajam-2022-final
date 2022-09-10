8b9d6f345793a4bcde60afdb0ca198f3c1eda69c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-09-10 09:13:11 +0900

    feat: update main.dart and app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'pages/home.dart';
+import 'pages/root_navigator.dart';
+import 'utils/extensions/build_context.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Note: アプリケーション起動時に匿名サインしたいときはコメントアウトを解除する。
+    // SPAJAM 2022 予選のソースコードも参照する。
+    // ref.read(signInAnonymouslyProvider)();
+    // final isSignedIn = ref.watch(isSignedInProvider).value ?? false;
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      key: UniqueKey(),
+      title: 'SPAJAM 2022 Final',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepOrange).copyWith(
+        textTheme: TextTheme(
+          displayLarge: context.textTheme.displayLarge!.copyWith(
+            color: Colors.black87,
+            fontSize: 28,
+            fontWeight: FontWeight.w700,
+          ),
+          displayMedium: context.textTheme.displayMedium!.copyWith(
+            color: Colors.black87,
+            fontSize: 26,
+            fontWeight: FontWeight.w700,
+          ),
+          displaySmall: context.textTheme.displaySmall!.copyWith(
+            color: Colors.black87,
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+          ),
+          headlineLarge: context.textTheme.headlineLarge!.copyWith(
+            color: Colors.black54,
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+          ),
+          headlineMedium: context.textTheme.headlineMedium!.copyWith(
+            color: Colors.black54,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+          headlineSmall: context.textTheme.headlineSmall!.copyWith(
+            color: Colors.black54,
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+          ),
+          titleLarge: context.textTheme.titleLarge!.copyWith(
+            color: Colors.black87,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+          titleMedium: context.textTheme.titleMedium!.copyWith(
+            color: Colors.black87,
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+          ),
+          titleSmall: context.textTheme.titleSmall!.copyWith(
+            color: Colors.black87,
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyLarge: context.textTheme.bodyLarge!.copyWith(
+            color: Colors.black87,
+            fontSize: 16,
+          ),
+          bodyMedium: context.textTheme.bodyMedium!.copyWith(
+            color: Colors.black87,
+            fontSize: 14,
+          ),
+          bodySmall: context.textTheme.bodySmall!.copyWith(
+            color: Colors.black87,
+            fontSize: 12,
+          ),
+          labelLarge: context.textTheme.labelLarge!.copyWith(
+            color: Colors.black54,
+            fontSize: 16,
+          ),
+          labelMedium: context.textTheme.labelMedium!.copyWith(
+            color: Colors.black54,
+            fontSize: 14,
+          ),
+          labelSmall: context.textTheme.labelSmall!.copyWith(
+            color: Colors.black54,
+            fontSize: 12,
+          ),
+        ),
+        cardTheme: const CardTheme(margin: EdgeInsets.zero),
+        scaffoldBackgroundColor: Colors.white,
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.black12,
+        ),
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const RootNavigator(),
+      builder: (context, child) {
+        return MediaQuery(
+          // 端末依存のフォントスケールを 1 に固定
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+          child: child!,
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'app.dart';
+import 'utils/application_documents_directory.dart';
+import 'utils/firebase_messaging.dart';
 import 'utils/firebase_options.dart';
+import 'utils/setup_local_emulator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,5 +17,24 @@ Future<void> main() async {
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // Firebase を初期化する。
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const App());
+  runApp(
+    ProviderScope(
+      overrides: <Override>[
+        applicationDocumentsDirectoryProvider.overrideWithValue(
+          await getApplicationDocumentsDirectory(),
+        ),
+        firebaseMessagingProvider.overrideWithValue(
+          await getFirebaseMessagingInstance,
+        ),
+      ],
+      child: const App(),
+    ),
+  );
+}
+
+/// 各種サービス関係での初期化処理を行う。
+Future<void> initialize() async {
+  await Future.wait<void>([
+    if (const String.fromEnvironment('FLAVOR') == 'local') setUpLocalEmulator(),
+  ]);
 }
