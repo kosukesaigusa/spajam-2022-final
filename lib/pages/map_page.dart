5f9202a1dd4608131b7d2af053581ae985ad1e3e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-11-19 18:42:56 +0900

    add pages

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
