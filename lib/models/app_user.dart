83108089e1934a5a060889214d67b3423992f99e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-11-20 03:51:12 +0900

    feat: country

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../utils/json_converters/country.dart';
+import '../utils/json_converters/flags.dart';
 import 'firestore_position.dart';
 
 part 'app_user.freezed.dart';
@@ -12,13 +14,13 @@ class AppUser with _$AppUser {
     @Default('') String name,
     @Default('') String appUserId,
     @Default('') String imageUrl,
-    @Default('') String country,
+    @CountryConverter() @Default(Country.unknown) Country country,
 
     /// マップ上に表示されるかどうか
     @Default(true) bool isVisible,
 
     /// 交流した人の国旗
-    @Default(<String>[]) List<String> flags,
+    @FlagsConverter() @Default(<Country>[]) List<Country> flags,
     @Default('') String comment,
     @FirestorePositionConverter()
     @Default(FirestorePosition.defaultValue)
@@ -26,7 +28,8 @@ class AppUser with _$AppUser {
     @Default(<String>[]) List<String> fcmTokens,
   }) = _AppUser;
 
-  factory AppUser.fromJson(Map<String, dynamic> json) => _$AppUserFromJson(json);
+  factory AppUser.fromJson(Map<String, dynamic> json) =>
+      _$AppUserFromJson(json);
 
   factory AppUser.fromDocumentSnapshot(DocumentSnapshot ds) {
     final data = ds.data()! as Map<String, dynamic>;
@@ -37,4 +40,15 @@ class AppUser with _$AppUser {
   }
 
   const AppUser._();
+}
+
+enum Country {
+  japan,
+  korea,
+  china,
+  france,
+  spain,
+  america,
+  brazil,
+  unknown,
 }
