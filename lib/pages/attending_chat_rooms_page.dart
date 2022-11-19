53ece819c676a2bdf9b3489cfbe2d6b8114e7504
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-11-19 19:49:58 +0900

    feat: add required pages

---
+++ lib/pages/attending_chat_rooms_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AttendingChatRoomsPage extends HookConsumerWidget {
+  const AttendingChatRoomsPage({super.key});
+
+  static const path = '/attendingChatRooms';
+  static const name = 'AttendingChatRoomsPage';
+  static const location = path;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const SizedBox();
+  }
+}
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends HookWidget {
   const MapPage({super.key});
+
+  static const path = '/map';
+  static const name = 'MapPage';
+  static const location = path;
 
   @override
   Widget build(BuildContext context) {
