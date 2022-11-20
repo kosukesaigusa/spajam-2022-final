00ab4715babbcc3b08b67fb4bad09daa82ea235c
heyhey1028 <shoheiogawa1028@gmail.com>
2022-11-20 09:21:04 +0900

    feat: photo compress

--- lib/repositories/firebase_storage_repository.dart
+++ lib/repositories/firebase_storage_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final firebaseStorageRepositoryProvider =
@@ -16,11 +17,47 @@ class FirebaseStorageRepository {
   }) async {
     final storageRef = _firebaseStorage.ref().child(path);
     try {
-      final uploadTask = storageRef.putData(uint8list);
+      final compressedData = await _compressImageFromList(uint8list);
+      final uploadTask = storageRef.putData(compressedData);
       final snapshot = await Future.value(uploadTask);
       return await snapshot.ref.getDownloadURL();
     } on FirebaseException {
       rethrow;
+    }
+  }
+
+  static Future<Uint8List> _compressImageFromList(
+    Uint8List data, {
+    int limitSize = 204800,
+  }) async {
+    if (data.lengthInBytes <= limitSize) {
+      return data;
+    }
+
+    final qualities = <int>[90, 80, 70, 60, 50, 40, 30, 20, 10, 5];
+    try {
+      late Uint8List bytes;
+
+      for (final q in qualities) {
+        final compressedBytes = await FlutterImageCompress.compressWithList(
+          data,
+          minHeight: 200,
+          minWidth: 200,
+          quality: q,
+        );
+
+        if (compressedBytes.lengthInBytes <= limitSize) {
+          bytes = compressedBytes;
+          break;
+        }
+        if (q == qualities.last) {
+          bytes = compressedBytes;
+        }
+      }
+
+      return bytes;
+    } on Exception catch (e, stackTrace) {
+      throw Exception('画像の圧縮に失敗しました。error: $e, stackTrace: $stackTrace');
     }
   }
 }
